42b42bef39bde8db06f03d382ba1806e8662032c
harshith-eth <vharshith.2810@gmail.com>
2024-07-02 21:30:16 +0530

    commit message

---
+++ server.js
@@ -0,0 +1,20 @@
+// server/server.js
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  socket.on('callUser', (data) => {
+    io.to(data.userToCall).emit('callUser', { signal: data.signalData, from: data.from });
+  });
+
+  socket.on('acceptCall', (data) => {
+    io.to(data.to).emit('callAccepted', data.signal);
+  });
+});
+
+server.listen(4000, () => console.log('Server is running on port 4000'));
