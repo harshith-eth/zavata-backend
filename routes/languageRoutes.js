c4717797cc3569a7e8f3d5deb4c3d2c2e8175a3d
harshith-eth <vharshith.2810@gmail.com>
2024-08-17 19:07:20 +0530

    Getting started with AWS

--- routes/languageRoutes.js
+++
@@ -1,8 +0,0 @@
-// routes/languageRoutes.js
-const express = require('express');
-const router = express.Router();
-const languageController = require('../controllers/languageController');
-
-router.post('/process', languageController.processText);
-
-module.exports = router;
